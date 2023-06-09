89d6e9befb8e79acf83a50986b11a142849a8432
Muhammet Ali KAYA <info@beemeapp.com>
2023-06-09 14:14:47 +0300

    08.06.2023 Gün sonu

--- src/main/java/com/muhammet/controller/PersonelDepartmanController.java
+++ src/main/java/com/muhammet/controller/PersonelDepartmanController.java
@@ -1,6 +1,8 @@
 package com.muhammet.controller;
 
 import com.muhammet.dto.request.GetNameUpperCaseRequestDto;
+import com.muhammet.exceptions.ErrorType;
+import com.muhammet.exceptions.Java8StartException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +14,31 @@ public class PersonelDepartmanController {
 
     @PostMapping("/getname")
     public ResponseEntity<String> getNameUpperCase(@RequestBody GetNameUpperCaseRequestDto dto){
+        if(dto.getName()==null)
+            throw new Java8StartException(ErrorType.NAME_IS_NULL);
         String nameUpper = dto.getName().toUpperCase();
         return ResponseEntity.ok(nameUpper);
     }
+
+
+    @GetMapping("/test")
+    public void test(){
+
+        int s1= 1;
+        int s2= 0;
+        String ifade = null;
+        try{
+            ifade.toUpperCase();
+            int sonuc = s1/s2;
+        }catch (ArithmeticException ex){
+            System.out.println("İşlem hatası oluştu....: "+ ex.toString());
+        } catch (NullPointerException ex){
+            System.out.println("Girilen değer null olamaz....: "+ ex.toString());
+        }
+
+
+
+
+    }
+
 }
